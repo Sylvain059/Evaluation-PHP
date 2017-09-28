68f0d3111b60eda2eafd5503604c06829547ce05
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-28 14:52:24 +0200

    add boostraps files & create head.php, header.php script.php & index.php

---
+++ views/head.php
@@ -0,0 +1,10 @@
+<head>
+	<meta charset="utf8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- FONTS LINK -->
+
+	<!-- CSS LINK -->
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+</head>
