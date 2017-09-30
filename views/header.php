5a511ce675a860369d64424284989f793cecb208
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-30 22:09:28 +0200

    header.php // add header tag

--- views/header.php
+++ views/header.php
@@ -1,5 +1,5 @@
-<header>
-    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+<header class="container-fluid">
+    <nav class="navbar navbar-toggleable-md navbar-inverse container">
 
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
