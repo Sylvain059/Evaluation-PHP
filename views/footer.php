81053f7bbdfb94b0d9faa116470b202e7195649d
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-30 22:15:59 +0200

    footer.php // creation of the footer & add 2 <p> with link

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+<footer class="container-fluid">
+
+    <div class="container">
+        <div class="row">
+            <hr>
+            <div class="col-lg-12">
+                <div class="col-lg-12">
+                    <a href="#">Terms of Service</a> | <a href="#">Privacy</a>
+                </div>
+                <div class="col-lg-12">
+                    <p class="muted pull-right copyright">© 2017 Company Name. All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
