4b9fb0d4b01db4f0c2865963eef3a7db2e8bcb3a
Sylvain059 <sylvain.sculier88@gmail.com>
2017-10-01 13:48:40 +0200

    head.php // add favicon link

--- views/head.php
+++ views/head.php
@@ -1,10 +1,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!--  FAVICON LINK  -->
+    <link rel="icon" type="image/png" href="../img/logo.png">
     <!--  FONT LINK  -->
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
-
     <!--  CSS LINK  -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
