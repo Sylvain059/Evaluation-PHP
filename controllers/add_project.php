b3a67f374a2af3d5995c3be2674f567959c38669
Sylvain059 <sylvain.sculier88@gmail.com>
2017-10-02 15:54:20 +0200

    creation controllers folder

---
+++ controllers/add_project.php
@@ -0,0 +1,11 @@
+<?php
+// Connection DB
+include '../models/connection_bdd.php';
+
+
+//Data Processing
+
+
+//view of the viewiew of the view
+include '../views/add_project.php';
+?>
---
+++ controllers/index.php
@@ -0,0 +1,12 @@
+<?php
+
+//I recuperate all projects
+    include '../models/connection_bdd.php';
+    include'../models/db_card.php';
+
+//Data processing.
+
+
+// I display the projects on the view.
+    include '../views/index.php';
+?>
