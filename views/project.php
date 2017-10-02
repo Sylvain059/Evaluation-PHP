9752e2711d7a30147d85327d6bb39d31b2ad976c
Sylvain059 <sylvain.sculier88@gmail.com>
2017-10-02 09:56:58 +0200

    creation project.php file & add include

---
+++ views/project.php
@@ -0,0 +1,20 @@
+<?php include '../models/connection_bdd.php'; ?>
+<!DOCTYPE html>
+<html>
+<?php include 'head.php';?>
+
+<body>
+    <?php include 'header.php';?>
+    <main class="container jumbotron">
+        <h2>Titre</h2>
+        <p>Description :</p>
+        <p>Deadline :</p>
+        <h3> Tasks :</h3>
+    </main>
+
+    <?php include 'footer.php';?>
+    <?php include 'script.php';?>
+
+</body>
+
+</html>
