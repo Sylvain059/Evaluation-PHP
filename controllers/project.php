966d179f494c9285dcf3ebd65d3ef5d25787756f
Sylvain059 <sylvain.sculier88@gmail.com>
2017-10-02 16:56:30 +0200

    add include for models & views

---
+++ controllers/project.php
@@ -0,0 +1,10 @@
+<?php
+include '../models/connection_bdd.php';
+include '../models/db_card.php';
+include '../models/db_tasks.php';
+//include '../models/table_join.php';
+include '../models/details_project.php';
+
+
+include'../views/project.php';
+?>
